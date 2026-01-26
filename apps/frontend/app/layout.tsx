ee1a538dbae7861ed9a2f80daa74de315458198b
alpadev-fx <founder@alpadev.xyz>
2026-01-26 18:48:08 -0500

    feat: add Google Tag Manager (GTM)

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -61,6 +61,16 @@ export default function RootLayout({
             gtag('config', '${GA_MEASUREMENT_ID}');
           `}
         </Script>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K9JTG7BN');
+          `}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
@@ -79,6 +89,15 @@ export default function RootLayout({
           '--font-mono': '"Fira Code", monospace',
         } as React.CSSProperties}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K9JTG7BN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Providers attribute="class" defaultTheme="dark">
           <div className="relative flex flex-col min-h-screen">
             <Navbar />
